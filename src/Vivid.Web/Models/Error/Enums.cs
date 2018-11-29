692f7d30c9e02aeb445c1b65a751e5e7262692c9
Poulad Ashrafpour <poulad.pld@gmail.com>
2018-11-28 20:43:14 -0500

    Update web app docs

--- src/Vivid.Web/Models/Error/Enums.cs
+++ src/Vivid.Web/Models/Error/Enums.cs
@@ -2,10 +2,19 @@
 
 namespace Vivid.Web
 {
-    public static partial class Enums
+    /// <summary>
+    /// Contains the constant values used on the web API
+    /// </summary>
+    public static class Enums
     {
+        /// <summary>
+        /// Contains the error code used
+        /// </summary>
         public static class ErrorCodes
         {
+            /// <summary>
+            /// Denotes a request validation error
+            /// </summary>
             public const string Validation = "err.req.validations";
         }
     }
